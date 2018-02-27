7e560a20095d7d1d55e932396882ad0d1924cf0f
Felipe Mateus <contato@felipefm32.com.br>
2018-02-27 16:51:57 -0300

    image

--- index.js
+++ index.js
@@ -1,50 +1,91 @@
 const express = require('express');
 const app = express();
-const request = require('sync-request');
-
-var metafetch = require('metafetch');
+const request = require('request');
+const { URL } = require('url');
+const  cheerio = require('cheerio');
+const sync_request = require('sync-request');
 
 const port = process.env.PORT || 8080;
 
 app.get('/Fetch', function(req, res) {
 
 		res.writeHead(200, {'Content-Type': 'application/json','Access-Control-Allow-Origin': '*'});
-		metafetch.fetch(req.query.url, (err, meta)=> {
-			
-						
-			/*
-			console.log('title: ', meta.title);
-			console.log('description: ', meta.description);
-			console.log('type: ', meta.type);
-			console.log('url: ', meta.url);
-			console.log('ampURL: ', meta.ampURL);
-			console.log('siteName: ', meta.siteName);
-			console.log('charset: ', meta.charset);
-			console.log('image: ', meta.image);
-			console.log('meta: ', meta.meta);
-			console.log('images: ', meta.images);
-			console.log('links: ', meta.links);
-			console.log('headers: ', meta.headers);
-			
-			res.status(200).send(JSON.stringify(meta));*/
+				
 		
-			let result = {};
-			
-			result.title = meta.title;
-			result.description = meta.description;
-			result.url = meta.url;
-			result.siteName = meta.siteName;
+		
+		  //make a new request to the URL provided in the HTTP POST request
+		request(req.query.url, function (error, response, responseHtml) {
+			var resObj = {};
 
-						
+			//if there was an error
+			if (error) {
+				res.end(JSON.stringify({error: 'There was an error of some kind'}));
+				return;
+			}
+
+			//create the cheerio object
+			resObj = {},
+				//set a reference to the document that came back
+				$ = cheerio.load(responseHtml),
+				//create a reference to the meta elements
+				$title = $('head title').text(),
+				$desc = $('meta[name="description"]').attr('content'),
+				$kwd = $('meta[name="keywords"]').attr('content'),
+				$ogTitle = $('meta[property="og:title"]').attr('content'),
+				$ogImage = $('meta[property="og:image"]').attr('content'),
+				$ogkeywords = $('meta[property="og:keywords"]').attr('content'),
+				$images = $('img');
+				
+			resObj.url = req.query.url;
+
+			if ($title) {
+				resObj.title = $title;
+			}
+
+			if ($desc) {
+				resObj.description = $desc;
+			}
+
+			if ($kwd) {
+				resObj.keywords = $kwd;
+			}
+
+			if ($ogImage && $ogImage.length){
+				let matches = $ogImage.match(/\bhttps?:\/\/\S+/gi);
+				
+				if(matches!=null && matches[0]!=null){
+					resObj.image = sync_request('GET', $ogImage).getBody().toString('base64');
+				}else{
+					let url = new URL(req.query.url);
+					url.pathname = $ogImage;
+					resObj.image = sync_request('GET', url.toString()).getBody().toString('base64');
+				}
+			}
+
+			/*if ($ogTitle && $ogTitle.length){
+				resObj.ogTitle = $ogTitle;
+			}
+
+			if ($ogkeywords && $ogkeywords.length){
+				resObj.ogkeywords = $ogkeywords;
+			}
+
+			if ($images && $images.length){
+				resObj.images = [];
+
+				for (var i = 0; i < $images.length; i++) {
+					let url = new URL(req.query.url);
+					url.pathname = $($images[i]).attr('src');
+					resObj.images.push(url);
+				}
+			}*/
+
+			//send the response
+			res.end(JSON.stringify(resObj));
+    }) ;
 			
-			result.image = {};
-			result.image.url = meta.image;
-			result.image.data =request('GET', meta.image).getBody().toString('base64');
-						
 			
-			res.write(JSON.stringify(result));
-			res.end();
-		});
+		
 });
 
 
